c46d833cfa12bbb06784ad822bf160da8131e3a0
Sijin.John <sijin.john@acsiatech.com>
2020-11-24 18:46:30 +0530

    Added a class

--- demo.py
+++ demo.py
@@ -1,1 +1,17 @@
 print("Hello")
+
+
+## i am adding a  new class
+
+class a:
+    def __init__(self):
+        self.b=10
+        print("initialized class a")
+    def fn(self):
+        self.b = self.b +10
+        print("b=",self.b)
+
+apple=a()
+apple.fn()
+
+
